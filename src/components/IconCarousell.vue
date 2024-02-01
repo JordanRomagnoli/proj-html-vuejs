a841b058ee55e77d7af5da5c211d2ea8f5607b18
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-02-01 15:26:05 +0100

    - ADD style alla NotFoundPage

--- src/components/IconCarousell.vue
+++ src/components/IconCarousell.vue
@@ -82,21 +82,7 @@ export default {
     }
 }
 
-.swiper {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 0;
-}
 
-.swiper-slide {
-    text-align: center;
-    @include flex-center;
-}
 
 .icon-frame {
     width: 150px;
--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -9,11 +9,27 @@
     </script>
 
 <template>
-    <h1>
-        Not Found
-    </h1>
+    <div class="container">
+        <div class="frame">
+            <h1>
+                Error 404 
+            </h1>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-   
+   .container{
+    min-height: 1200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .frame{
+        padding: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: lightgray;
+    }
+   }
 </style>
